59b7c160a8facc79561faada94791f841f4e6b0d
alsoj <alsoj@naver.com>
2022-11-23 19:17:51 +0900

    인스타 크롤링 개선(댓글 추출)

---
+++ instagram/Comment.py
@@ -0,0 +1,165 @@
+from time import sleep
+
+from PyQt6.QtCore import QThread
+import json
+
+from selenium.webdriver.common.by import By
+import Config
+import unicodedata
+from openpyxl import load_workbook
+from Common import get_app_id, get_short_code
+import requests
+
+class CommentWorker(QThread):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+
+    def run(self):
+        # 로그인 된 이후에 실행되어야 함
+        code_list = []
+        if self.parent.crawl_type == 'recent':
+            code_list = get_recent_post(self.parent.browser, self.parent.edit_recent.text())
+        else:
+            code_list = self.get_target_post()
+
+        total_user = {}
+        crawl_count = 0
+        for index, code in enumerate(code_list):
+            if len(code) > 0:
+                crawl_count += 1
+                sheet_num = index + 1
+                self.parent.info(f"{sheet_num}번 째 게시글 추출을 시작합니다.")
+                user_list = self.get_likers(self.parent.browser, code, sheet_num)
+                for user in user_list:
+                    if user in total_user.keys():
+                        total_user[user] += 1
+                    else:
+                        total_user[user] = 1
+
+        file_path_name = Config.FILE_PATH + self.parent.file_name
+        common_user = [k for k, v in total_user.items() if v == crawl_count]
+        wb = load_workbook(file_path_name, data_only=True)
+        ws = wb['공통 사용자']
+        rownum = 0
+        for user in common_user:
+            rownum += 1
+            ws.append([rownum, user])
+        wb.save(file_path_name)
+
+        if self.parent.crawl_type == 'recent':
+            self.parent.info("최근 게시물 크롤링이 종료 되었습니다.")
+        else:
+            self.parent.info("특정 게시물 크롤링이 종료 되었습니다.")
+        self.parent.button_activate(True)
+
+    def get_target_post(self):
+        url1 = get_short_code(self.parent.edit_url1.text())
+        url2 = get_short_code(self.parent.edit_url2.text())
+        url3 = get_short_code(self.parent.edit_url3.text())
+        url4 = get_short_code(self.parent.edit_url4.text())
+        url5 = get_short_code(self.parent.edit_url5.text())
+        url6 = get_short_code(self.parent.edit_url6.text())
+        code_list = [url1, url2, url3, url4, url5, url6]
+        return code_list
+
+    # 댓글 추출
+    def get_comments(self, browser, short_code):
+        end_cursor = ''
+        variables = {'shortcode': short_code, 'first': 50, 'after': end_cursor}
+        has_next_page = True
+        rownum = 0
+        file_path_name = Config.FILE_PATH + self.parent.file_name
+        wb = load_workbook(file_path_name, data_only=True)
+        user_list = []
+
+        while has_next_page:
+            variables['after'] = end_cursor
+            json_variables = str(json.dumps(variables))
+            url = f'{Config.COMMENT_URL}&variables={json_variables}'
+
+            browser.get(url)
+            sleep(3)
+
+            content = browser.find_element(by=By.TAG_NAME, value='pre').text
+            data = json.loads(content)
+
+            if 'data' in data:
+                has_next_page = data['data']['shortcode_media']['edge_media_to_parent_comment']['page_info']['has_next_page']
+                end_cursor = data['data']['shortcode_media']['edge_media_to_parent_comment']['page_info']['end_cursor']
+                comments = data['data']['shortcode_media']['edge_media_to_parent_comment']['edges']
+
+                for comment in comments:
+                    rownum += 1
+                    #             write_excel('comments', [rownum, 1, comment['node']['owner']['username'], comment['node']['text']])
+                    print(rownum, 1, comment['node']['owner']['username'], comment['node']['text'])
+                    co_comments = comment['node']['edge_threaded_comments']['edges']
+                    for co_comment in co_comments:
+                        #                 write_excel('comments', [rownum, 2, co_comment['node']['owner']['username'], co_comment['node']['text']])
+                        print(rownum, 2, co_comment['node']['owner']['username'], co_comment['node']['text'])
+
+                    if rownum % 100 == 0:
+                        print(f'댓글 {rownum}개 추출 완료')
+            else:
+                has_next_page = False
+                self.parent.error("비정상적인 응답이 발생했습니다. 접속 계정 제한 여부를 확인해주세요.")
+
+        while has_next_page:
+            variables['after'] = end_cursor
+            json_variables = str(json.dumps(variables))
+            url = f'{Config.LIKER_URL}&variables={json_variables}'
+
+            browser.get(url)
+            sleep(3)
+            content = browser.find_element(by=By.TAG_NAME, value='pre').text
+            data = json.loads(content)
+
+            if 'data' in data:
+                has_next_page = data['data']['shortcode_media']['edge_liked_by']['page_info']['has_next_page']
+                end_cursor = data['data']['shortcode_media']['edge_liked_by']['page_info']['end_cursor']
+                likers = data['data']['shortcode_media']['edge_liked_by']['edges']
+
+                for liker in likers:
+                    rownum += 1
+                    user_list.append(liker['node']['username'])
+                    ws.append([short_code, rownum, liker['node']['username'], unicodedata.normalize('NFC', liker['node']['full_name'])])
+                    if rownum % 50 == 0:
+                        self.parent.debug(f'{rownum}번째 데이터 추출 완료')
+                        wb.save(file_path_name)
+            else:
+                has_next_page = False
+                self.parent.error("비정상적인 응답이 발생했습니다. 접속 계정 제한 여부를 확인해주세요.")
+
+        wb.save(file_path_name)
+
+        return user_list
+
+
+def get_recent_post(browser, user_id):
+    timeline_url = f'{Config.BASE_URL}' + user_id
+
+    browser.get(timeline_url)
+    page_source = browser.page_source
+    app_id = get_app_id(page_source)
+    csrftoken = browser.get_cookie("csrftoken")['value']
+
+    cookies = browser.get_cookies()
+    header_cookie = ''
+    for cookie in cookies:
+        header_cookie += f"{cookie['name']}={cookie['value']}; "
+
+    # 헤더 세팅
+    headers = {'referer': Config.BASE_URL, 'x-csrftoken': csrftoken, 'cookie': header_cookie, 'x-ig-app-id': app_id}
+    api_url = f'https://www.instagram.com/api/v1/feed/user/{user_id}/username/?count=12'
+    res = requests.get(api_url, headers=headers)
+    data = res.json()
+
+    code_list = [
+                data['items'][0]['code'],
+                data['items'][1]['code'],
+                data['items'][2]['code'],
+                data['items'][3]['code'],
+                data['items'][4]['code'],
+                data['items'][5]['code']
+                ]
+    return code_list
---
+++ instagram/LikeCommentCrawler.py
@@ -0,0 +1,181 @@
+import os
+import sys
+from time import sleep
+
+from PyQt6.QtWidgets import *
+from PyQt6 import uic
+
+from datetime import datetime
+from Common import execute_browser
+from openpyxl import Workbook
+
+import Config
+from Liker import LikerWorker
+from selenium.webdriver.common.by import By
+
+form_class = uic.loadUiType("LikeCommentCrawler.ui")[0]
+class INSTA_Window(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.btn_start_recent.clicked.connect(self.btn_start_recent_clicked)  # 최근 게시물 크롤링
+        self.btn_start_target.clicked.connect(self.btn_start_target_clicked)  # 특정 게시물 크롤링
+        self.scroll_bar = self.log_browser.verticalScrollBar()
+        self.log_browser.append(" - 추출 결과는 output 폴더 하위에 저장됩니다.")
+        self.log_browser.append(" - 게시글 URL 예시) https://www.instagram.com/p/Cf-vUAcLS8K/")
+        self.log_browser.append(" - 과도한 크롤링은 인스타그램에서 제한될 수 있습니다.")
+        self.browser = execute_browser()
+        self.liker_worker = LikerWorker(self)
+        self.login_id = None
+        self.login_pw = None
+        self.file_name = None
+        self.crawl_type = None
+
+    # 최근 게시물 클릭
+    def btn_start_recent_clicked(self):
+        self.info("최근 게시물 좋아요 추출을 시작합니다.")
+        self.button_activate(False)
+        self.set_id_pw()
+        if self.login():
+            self.set_file_name('recent')
+            create_excel(self.file_name)
+            self.crawl_type = 'recent'
+            self.liker_worker.start()
+        else:
+            self.button_activate(True)
+
+    # 특정 게시물 클릭
+    def btn_start_target_clicked(self):
+        self.info("특정 게시물 좋아요 추출을 시작합니다.")
+        self.button_activate(False)
+        self.set_id_pw()
+        if self.login():
+            self.set_file_name('target')
+            create_excel(self.file_name)
+            self.crawl_type = 'target'
+            self.liker_worker.start()
+        else:
+            self.button_activate(True)
+
+    # 로그인 처리
+    def login(self):
+        if self.login_id is None or self.login_pw is None \
+                or len(self.login_id) == 0 or len(self.login_pw) == 0:
+            self.error("인스타그램 ID와 PW를 입력해주세요.")
+            return False
+
+        self.browser.get(Config.LOGIN_URL)
+        sleep(2)
+
+        if 'accounts' in self.browser.current_url:
+            inputs = self.browser.find_elements(by=By.TAG_NAME, value='input')
+            inputs[0].clear()
+            inputs[1].clear()
+            inputs[0].send_keys(self.login_id)
+            inputs[1].send_keys(self.login_pw)
+            inputs[1].submit()
+            sleep(2)
+
+            if '잘못된 비밀번호' in self.browser.page_source:
+                self.error('잘못된 비밀번호입니다.')
+                return False
+            elif '입력한 사용자 이름' in self.browser.page_source:
+                self.error('잘못된 사용자 ID입니다.')
+                return False
+            elif '문제가 발생' in self.browser.page_source:
+                self.error('일시적인 문제가 발생하였습니다.')
+                return False
+            else:
+                self.info(f'로그인에 성공했습니다. ID : {self.login_id}')
+                return True
+
+        else:  # 이미 로그인된 상태
+            return True
+
+    # ID/PW 세팅
+    def set_id_pw(self):
+        self.login_id = self.edit_id.text()
+        self.login_pw = self.edit_pw.text()
+
+    # 파일명 세팅
+    def set_file_name(self, crawl_type):
+        input_file_name = self.edit_file_name.text()
+        if len(input_file_name) > 0:
+            file_name = input_file_name + '.xlsx'
+        else:
+            now = datetime.now().strftime('%Y%m%d%H%M%S')
+            type_name = '최근' if crawl_type == 'recent' else '특정'
+            file_name = f'{now}_{type_name} 게시물 좋아요 추출.xlsx'
+
+        self.file_name = file_name
+
+    # 버튼 비활성화
+    def button_activate(self, enable):
+        self.btn_start_recent.setEnabled(enable)
+        self.btn_start_target.setEnabled(enable)
+
+    # 디버그 로그 출력
+    def debug(self, text):
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.log_browser.append(f'<p>[{now}] {text}</p>')
+        self.scroll_bar.setValue(self.scroll_bar.maximum())
+
+    # 정보 로그 출력(초록색)
+    def info(self, text):
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.log_browser.append(f'<p style="color:green">[{now}] {text}</p>')
+        self.scroll_bar.setValue(self.scroll_bar.maximum())
+
+    # 에러 로그 출력(빨간색)
+    def error(self, text):
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.log_browser.append(f'<p style="color:red">[{now}] {text}</p>')
+        self.scroll_bar.setValue(self.scroll_bar.maximum())
+
+    # 종료 이벤트
+    def closeEvent(self, QCloseEvent):
+        if hasattr(self, 'browser') and self.browser is not None:
+            self.browser.quit()
+
+def create_excel(file_name):
+    wb = Workbook()
+    wb.create_sheet('게시글1', 0)
+    wb.create_sheet('게시글2', 1)
+    wb.create_sheet('게시글3', 2)
+    wb.create_sheet('게시글4', 3)
+    wb.create_sheet('게시글5', 4)
+    wb.create_sheet('게시글6', 5)
+    wb.create_sheet('공통 사용자', 6)
+
+    ws1 = wb['게시글1']
+    ws2 = wb['게시글2']
+    ws3 = wb['게시글3']
+    ws4 = wb['게시글4']
+    ws5 = wb['게시글5']
+    ws6 = wb['게시글6']
+    ws7 = wb['공통 사용자']
+
+    sub_liker = ['구분', '번호', '사용자ID', '사용자명']
+    for kwd, j in zip(sub_liker, list(range(1, len(sub_liker) + 1))):
+        ws1.cell(row=1, column=j).value = kwd
+        ws2.cell(row=1, column=j).value = kwd
+        ws3.cell(row=1, column=j).value = kwd
+        ws4.cell(row=1, column=j).value = kwd
+        ws5.cell(row=1, column=j).value = kwd
+        ws6.cell(row=1, column=j).value = kwd
+
+    sub_total = ['번호', '사용자ID']
+    for kwd, j in zip(sub_total, list(range(1, len(sub_total) + 1))):
+        ws7.cell(row=1, column=j).value = kwd
+
+    file_path = Config.FILE_PATH
+    if not os.path.isdir(file_path):
+        os.mkdir(file_path)
+    wb.save(file_path + file_name)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    INSTA = INSTA_Window()
+    INSTA.show()
+    app.exec()
